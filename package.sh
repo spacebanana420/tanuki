caa374c7a74e765270eb1e5806badf5c70494a0b
Space Banana <tomasbessa@protonmail.com>
2024-03-07 18:40:01 +0000

    Build script changes

--- package.sh
+++ package.sh
@@ -2,3 +2,9 @@ echo "Building lightweight JAR"
 scalac src/*.scala src/*/*.scala src/*/*/*.scala -d tanuki.jar
 echo "Building Fat JAR"
 steam-run scala-cli --power package src --assembly --preamble=false -f --jvm 8 -o tanuki-java.jar
+echo "Building native binary (Linux, static)"
+native-image --no-fallback --static -O3 -jar tanuki-java.jar -o tanuki-linux-x86_64
+echo "Building native binary (Linux, static, native cpu)"
+native-image --no-fallback --static -march=native -O3 -jar tanuki-java.jar -o tanuki-native
+echo "Packaging Linux native binary"
+7z a -mx8 -mmt0 tanuki-linux-x86_64.zip tanuki-linux-x86_64
